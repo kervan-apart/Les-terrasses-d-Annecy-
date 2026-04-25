b55637c4afebc43bba8b7f5fdb007d5882be5065
kervan-apart <pimo29@protonmail.com>
2026-04-25 14:50:36 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// ============================================================
-// CONFIGURATION GOOGLE SHEETS — Les Terrasses d'Annecy
-// Remplacez la valeur ci-dessous par votre identifiant Google Sheet
-// (voir guide-google-sheets.html pour les instructions)
-// ============================================================
-const SHEET_ID = 'COLLER_VOTRE_ID_ICI';
