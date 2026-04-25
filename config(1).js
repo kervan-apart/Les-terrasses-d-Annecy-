3cd2c3ec781ff8eae4cba02775ec1a72de7c94f3
kervan-apart <pimo29@protonmail.com>
2026-04-25 15:49:23 +0300

    Add files via upload

---
+++ config(1).js
@@ -0,0 +1,4 @@
+// ============================================================
+// CONFIGURATION GOOGLE SHEETS — Les Terrasses d'Annecy
+// ============================================================
+const SHEET_ID = '1tGBIel1pKA2qJWkDA_DpjInH4AugZxj0Y4DZplpjJjY';
