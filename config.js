3ca4e8c4e266a1b482afeea97e15853e25acd829
kervan-apart <pimo29@protonmail.com>
2026-04-25 18:48:19 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
 // ============================================================
 // CONFIGURATION GOOGLE SHEETS — Les Terrasses d'Annecy
 // ============================================================
-const SHEET_ID = '1tGBIel1pKA2qJWkDA_DpjInH4AugZxj0Y4DZplpjJjY';
+const SHEET_PUBLISHED_ID = '2PACX-1vQvrN_QDXFXyGAPrHldKdEWRrcqUrWYk3pxwlGnp1_IRld9lQa4mRCQ20HSzNFtX2kutrObEIzOj0E-';
+const GID_TARIFS = '1581779442';
+const GID_RESERVATIONS = '1808993612';
+
+const BASE_URL = `https://docs.google.com/spreadsheets/d/e/${SHEET_PUBLISHED_ID}/pub?output=csv`;
+const URL_TARIFS       = BASE_URL + `&gid=${GID_TARIFS}`;
+const URL_RESERVATIONS = BASE_URL + `&gid=${GID_RESERVATIONS}`;
